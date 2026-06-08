74ec6cad780ba9fd1c40a448da26641ab444f438
ClairvoyantHG <wjdgory@gmail.com>
2026-06-09 05:17:15 +0900

    [아이템] 아이템 UI 구성

--- Assets/Scripts/UI/InventoryUI.cs
+++ Assets/Scripts/UI/InventoryUI.cs
@@ -1,9 +1,14 @@
 ﻿using UnityEngine;
+using TMPro;
 
 public class InventoryUI : UIBase
 {
     private UIButton btnClose;
     private UIButton btnGameStart;
+    private UIButton btnUseHpPlus;
+    private UIButton btnUseSpeedPlus;
+    private TextMeshProUGUI textItem01Amount;
+    private TextMeshProUGUI textItem02Amount;
 
     private void Awake()
     {
@@ -21,11 +26,42 @@ public class InventoryUI : UIBase
             {
                 btnGameStart = childButtons[i];
             }
+            else if (objName == "Button_Item_01")
+            {
+                btnUseHpPlus = childButtons[i];
+            }
+            else if (objName == "Button_Item_02")
+            {
+                btnUseSpeedPlus = childButtons[i];
+            }
         }
 
         if (btnClose == null || btnGameStart == null)
         {
             Debug.LogError("[InventoryUI] 하위 버튼의 이름(Button_Close, Button_Start)을 확인해주세요.");
+        }
+
+        TextMeshProUGUI[] childTexts = GetComponentsInChildren<TextMeshProUGUI>(true);
+
+        for (int i = 0; i < childTexts.Length; i++)
+        {
+            string objName = childTexts[i].gameObject.name;
+
+            if (objName == "Text_Item_01_Amount")
+            {
+                textItem01Amount = childTexts[i];
+                textItem01Amount.text = InventoryManager.Instance.CurrentHpMaterial.ToString();
+            }
+            else if (objName == "Text_Item_02_Amount")
+            {
+                textItem02Amount = childTexts[i];
+                textItem02Amount.text = InventoryManager.Instance.CurrentSpeedMaterial.ToString();
+            }
+        }
+
+        if (btnClose == null || btnGameStart == null)
+        {
+            Debug.LogError("[InventoryUI] 하위 버튼의 이름(Text_Item_01_Amount, Text_Item_02_Amount)을 확인해주세요.");
         }
     }
 
@@ -35,6 +71,8 @@ public class InventoryUI : UIBase
 
         if (btnClose != null) btnClose.BindOnClickButtonEvent(OnClickClose);
         if (btnGameStart != null) btnGameStart.BindOnClickButtonEvent(OnClickGameStart);
+        if (btnUseHpPlus != null) btnUseHpPlus.BindOnClickButtonEvent(OnClickUseHpPlus);
+        if (btnUseSpeedPlus != null) btnUseSpeedPlus.BindOnClickButtonEvent(OnClickUseSpeedPlus);
     }
 
     public override void OnClose()
@@ -43,6 +81,8 @@ public class InventoryUI : UIBase
 
         if (btnClose != null) btnClose.UnBindOnClickButtonEvent(OnClickClose);
         if (btnGameStart != null) btnGameStart.UnBindOnClickButtonEvent(OnClickGameStart);
+        if (btnUseHpPlus != null) btnUseHpPlus.UnBindOnClickButtonEvent(OnClickUseHpPlus);
+        if (btnUseSpeedPlus != null) btnUseSpeedPlus.UnBindOnClickButtonEvent(OnClickUseSpeedPlus);
     }
 
     private void OnClickClose()
@@ -55,5 +95,15 @@ public class InventoryUI : UIBase
         UIManager.Instance.CloseUI(UIType.InventoryUI);
         UIManager.Instance.CloseUI(UIType.TitleUI);
         UIManager.Instance.OpenUI(UIRootType.VeryFront, UIType.CountdownUI);
+    }
+
+    private void OnClickUseHpPlus()
+    {
+
+    }
+
+    private void OnClickUseSpeedPlus()
+    {
+
     }
 }
